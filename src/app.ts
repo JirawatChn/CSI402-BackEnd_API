e2d11832689d0d834909f62804f287f3898cbeda
Jirawat Chanasit <jirawat.chn@spumail.net>
2025-02-09 14:59:32 +0700

    third

--- src/app.ts
+++ src/app.ts
@@ -11,19 +11,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(helmet());
 
-const points = 1000;
+let points = 1000;
 
 app.post("/spumember/collectpoints", (req: Request, res: Response) => {
-  const { studentId,amount } = req.body;
-  if(!studentId || !amount) {
+  const { stdId,amount } = req.body;
+  if(!stdId || !amount) {
     res.status(400).json({
       status: 400,
       message: "Request body is invalid",
     });
   }else{
-
+    let studentId = stdId;
     let getPoints = amount/100;
-    let studentPoints = points + getPoints;
+    points = points + getPoints;
 
     res.status(200).json({
       status: 200,
@@ -31,7 +31,7 @@ app.post("/spumember/collectpoints", (req: Request, res: Response) => {
       data: {
         studentId,
         getPoints,
-        studentPoints
+        points
       }
     });
   }
