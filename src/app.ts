a16cb1c6fbbc706d10ec76fcfa5777df789fedfd
Jirawat Chanasit <jirawat.chn@spumail.net>
2025-02-09 14:43:09 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,41 @@
+import express, { Request, Response } from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+
+const points = 1000;
+
+app.post("/spumember/reward", (req: Request, res: Response) => {
+  const { studentId,amount } = req.body;
+  if(!studentId || !amount) {
+    res.status(400).json({
+      status: 400,
+      message: "Request body is invalid",
+    });
+  }else{
+
+    let getPoints = points + (amount/100)
+
+    res.status(200).json({
+      status: 200,
+      message: "Success",
+      data: {
+        studentId,
+        getPoints,
+        points
+      }
+    });
+  }
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+})
